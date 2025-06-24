103bd4825ffac33c7b2b5f728fc488952e298b29
Pier Fumagalli <pier@usrz.com>
2025-06-24 12:54:34 +0200

    Some basic configurability via `dotenv`

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli'
+import 'dotenv/config'
+
+/* Easy configuration via ".env" file */
+const codeServerUrl = process.env.CODE_SERVER_URL || 'https://code-server.usrz.com/'
+const applicationId = process.env.APPLICATION_ID || 'com.usrz.coder'
+const applicationName = process.env.APPLICATION_NAME || 'Coder'
+
+/* Validate and mangle the URL */
+const url = new URL(codeServerUrl)
+url.searchParams.set('ew', 'true')
 
 const config: CapacitorConfig = {
   /* The App ID: if you want TestFlight, change this! */
-  appId: 'com.usrz.coder',
+  appId: applicationId,
   /* The name of the app as shown in the dock */
-  appName: 'Coder',
+  appName: applicationName,
   /* Root directory of our web assets */
   webDir: 'public',
   /* IOS support */
@@ -16,14 +26,14 @@ const config: CapacitorConfig = {
   },
   server: {
     /* This is the server hosting the code-server instance */
-    url: 'https://code-server.usrz.com/?ew=true',
+    url: url.href,
     /* Allow navigation to these domains:
      * - code-server.usrz.com: the code-server instance
      * - *.cloudflareaccess.com: the Cloudflare Access login page
      * - *.google.com: the Google login page for Google SSO via Cloudflare
      */
     allowNavigation: [
-      'code-server.usrz.com',
+      url.hostname,
       '*.cloudflareaccess.com',
       '*.google.com',
     ],
